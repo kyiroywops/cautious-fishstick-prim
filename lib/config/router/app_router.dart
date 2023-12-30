9e09c28b1540c83a06c8644468c3fc05de792f4c
Kylian Leroy <aleexisazul@gmail.com>
2023-12-30 19:39:47 -0300

    inicial setup

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:piramjuego/presentation/screens/category_screen.dart';
+import 'package:piramjuego/presentation/screens/games_screen.dart';
 import 'package:piramjuego/presentation/screens/home_screen.dart';
+import 'package:piramjuego/presentation/screens/inicial_home_screen.dart';
+import 'package:piramjuego/presentation/screens/introductions_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
+        builder: (BuildContext context, GoRouterState state) => InicialHomeScreen(),
+        
+
+
+    ),
+    GoRoute(
+      path: '/playerselection',
         builder: (BuildContext context, GoRouterState state) => PlayerSelectionScreen(),
         
 
 
+    ),
+     GoRoute(
+      path: '/games',
+        builder: (BuildContext context, GoRouterState state) => GamesScreen(),
+      
+        
+
+
+    ),
+     GoRoute(
+      path: '/instructions',
+        builder: (BuildContext context, GoRouterState state) => InstructionsScreen(),
+      
+        
+
+
+    ),
+
+    GoRoute(
+      path: '/questions',
+      builder: (BuildContext context, GoRouterState state) {
+        // Obtenemos la categoría pasada como parámetro extra.
+        final category = state.extra as String;
+        // Luego, pasamos esta categoría a la pantalla correspondiente.
+        return QuestionsScreen(category: category);
+      },
     ),
 
 
---
+++ lib/presentation/providers/player_provider.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:piramjuego/infrastructure/models/player_models.dart';
+import 'package:piramjuego/presentation/providers/player_list_notifier.dart';
+
+final currentPlayerIndexProvider = StateProvider<int>((ref) => 0);
+
+final playerProvider = StateNotifierProvider<PlayerListNotifier, List<Player>>((ref) {
+  return PlayerListNotifier();
+});

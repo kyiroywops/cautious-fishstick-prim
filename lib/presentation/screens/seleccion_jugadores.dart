c27981ff6b47c05b2446d64024f67e2276a733da
Kylian Leroy <aleexisazul@gmail.com>
2024-01-23 00:49:10 -0300

    ui select

--- lib/presentation/screens/seleccion_jugadores.dart
+++ lib/presentation/screens/seleccion_jugadores.dart
@@ -55,6 +55,9 @@ class _PlayerSelectionScreenState extends ConsumerState<PlayerSelectionScreen> {
       ref.read(playerProvider.notifier).removePlayer(index);
     }
 
+    final Size screenSize = MediaQuery.of(context).size;
+
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onBackground,
       appBar: AppBar(
@@ -82,196 +85,209 @@ class _PlayerSelectionScreenState extends ConsumerState<PlayerSelectionScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(40, 0, 40, 110),
-        child: Column(
-          children: [
-           
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 35, 10, 0),
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7, // Ajusta según el diseño de tu UI
-                    crossAxisSpacing: 10, // Espaciado horizontal
-                    mainAxisSpacing: 10, // Espaciado vertical
-                  ),
-                  itemCount: 21, // Asume que tienes 20 avatares
-                  itemBuilder: (context, index) {
-                    String avatarAsset =
-                        'assets/images/avatars/avatar${index + 1}.png';
-                    bool isSelected = _selectedAvatar == avatarAsset;
-                
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedAvatar =
-                              avatarAsset; // Actualiza el avatar seleccionado
-                        });
-                      },
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(40, screenSize.height * 0.02, 40, screenSize.height * 0.1),
+          
+              child: Column(
+                children: [
+                 
+               
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 35, 10, 0),
                       child: Container(
-                        decoration: isSelected
-                            ? BoxDecoration(
-                                border: Border.all(
-                                  color: Colors
-                                      .orange, // Color del borde cuando está seleccionado
-                                  width: 3, // Ancho del borde
-                                ),
-                                shape: BoxShape
-                                    .circle, // Forma circular para el borde
-                              )
-                            : null,
-                        child: ClipOval(
-                          child: Image.asset(
-                            avatarAsset,
-                            width: 80, // Ajusta el tamaño del avatar
-                            height: 80, // Ajusta el tamaño del avatar
-                            fit: BoxFit
-                                .cover, // Esto asegura que la imagen llene el ClipOval
+                        height: screenSize.height * 0.3, // Ajusta esta altura según tus necesidades
+          
+                        child: GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 7, // Ajusta según el diseño de tu UI
+                            crossAxisSpacing: 10, // Espaciado horizontal
+                            mainAxisSpacing: 10, // Espaciado vertical
                           ),
+                          itemCount: 21, // Asume que tienes 20 avatares
+                          itemBuilder: (context, index) {
+                            String avatarAsset =
+                                'assets/images/avatars/avatar${index + 1}.png';
+                            bool isSelected = _selectedAvatar == avatarAsset;
+                        
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _selectedAvatar =
+                                      avatarAsset; // Actualiza el avatar seleccionado
+                                });
+                              },
+                              child: Container(
+                                decoration: isSelected
+                                    ? BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors
+                                              .orange, // Color del borde cuando está seleccionado
+                                          width: 3, // Ancho del borde
+                                        ),
+                                        shape: BoxShape
+                                            .circle, // Forma circular para el borde
+                                      )
+                                    : null,
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    avatarAsset,
+                                    width: 80, // Ajusta el tamaño del avatar
+                                    height: 80, // Ajusta el tamaño del avatar
+                                    fit: BoxFit
+                                        .cover, // Esto asegura que la imagen llene el ClipOval
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                       ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                    vertical:
-                        8.0), // Agrega márgenes verticales si es necesario
-                padding: EdgeInsets.symmetric(
-                    horizontal: 30.0,
-                    vertical:
-                        15.0), // Aumenta el padding para un container más grande
-                decoration: BoxDecoration(
-                  color: Colors.black
-                      .withOpacity(0.60), // Fondo negro para el container
-                  borderRadius: BorderRadius.circular(
-                      30), // Bordes redondeados para el container
-                ),
-                child: TextField(
-                  controller: _nameController,
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(
-                        20), // Limita la longitud del input a 20 caracteres
-                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z0-9]")),
-                  ],
-                  style: const TextStyle(
-                      color: Colors.white), // Texto blanco para el input
-                  cursorColor: Colors.white, // Color del cursor a blanco
-                  decoration: InputDecoration(
-                    hintText: 'Nombre del jugador',
-                    hintStyle: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'Lexend',
-                        fontWeight:
-                            FontWeight.w500), // Hint en blanco con opacidad
-                    border: InputBorder.none, // Sin borde
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    contentPadding: EdgeInsets.symmetric(
-                        vertical:
-                            10),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.add_circle, color: Colors.white),
-                      onPressed: players.length >= 20 ? null : _addPlayer,
-                    ), // Padding vertical para el texto dentro del input
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-           
-            
-            SizedBox(height: 30),
-            Center(
-              child: Text(
-                'Listado de Jugadores',
-                style: TextStyle(
-                  fontFamily: 'Lexend',
-                  color: Theme.of(context).colorScheme.background,
-                  fontWeight:
-                      FontWeight.w700, // Si deseas que el texto esté en negrita
-                  fontSize:
-                      24, // Puedes ajustar el tamaño según tus necesidades
-                  // Otros estilos si son necesarios
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            Expanded(
-              child: ListView.builder(
-                itemCount: players
-                    .length, // Usar la longitud de la lista obtenida del provider
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(players[index].avatar),
                     ),
-                    title: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        players[index].name,
-                        style: TextStyle(color: Colors.white),
+                  
+                  
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical:
+                              8.0), // Agrega márgenes verticales si es necesario
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 30.0,
+                          vertical:
+                              15.0), // Aumenta el padding para un container más grande
+                      decoration: BoxDecoration(
+                        color: Colors.black
+                            .withOpacity(0.60), // Fondo negro para el container
+                        borderRadius: BorderRadius.circular(
+                            30), // Bordes redondeados para el container
+                      ),
+                      child: TextField(
+                        controller: _nameController,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(
+                              20), // Limita la longitud del input a 20 caracteres
+                          FilteringTextInputFormatter.allow(RegExp("[a-zA-Z0-9]")),
+                        ],
+                        style: const TextStyle(
+                            color: Colors.white), // Texto blanco para el input
+                        cursorColor: Colors.white, // Color del cursor a blanco
+                        decoration: InputDecoration(
+                          hintText: 'Nombre del jugador',
+                          hintStyle: TextStyle(
+                              color: Colors.white.withOpacity(0.5),
+                              fontFamily: 'Lexend',
+                              fontWeight:
+                                  FontWeight.w500), // Hint en blanco con opacidad
+                          border: InputBorder.none, // Sin borde
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical:
+                                  10),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add_circle, color: Colors.white),
+                            onPressed: players.length >= 20 ? null : _addPlayer,
+                          ), // Padding vertical para el texto dentro del input
+                        ),
                       ),
                     ),
-                    // Icono para eliminar jugadores
-                    trailing: IconButton(
-                      icon: Icon(Icons.remove_circle, color: Color(0xffFF414D)),
-                      onPressed: () {
-                        // Llama a la función para eliminar este jugador específico
-                        _removePlayer(index);
-                      },
+                  ),
+                  SizedBox(height: screenSize.height * 0.03),
+          
+                 
+                  
+                  SizedBox(height: screenSize.height * 0.03),
+          
+                  Center(
+                    child: Text(
+                      'Listado de Jugadores',
+                      style: TextStyle(
+                        fontFamily: 'Lexend',
+                        color: Theme.of(context).colorScheme.background,
+                        fontWeight:
+                            FontWeight.w700, // Si deseas que el texto esté en negrita
+                        fontSize:
+                            24, // Puedes ajustar el tamaño según tus necesidades
+                        // Otros estilos si son necesarios
+                      ),
                     ),
-                  );
-                },
+                  ),
+                  SizedBox(height: screenSize.height * 0.03),
+          
+                  
+                    Container(
+                      height: screenSize.height * 0.4,
+                      child: ListView.builder(
+                        itemCount: players
+                            .length, // Usar la longitud de la lista obtenida del provider
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: AssetImage(players[index].avatar),
+                            ),
+                            title: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                players[index].name,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            // Icono para eliminar jugadores
+                            trailing: IconButton(
+                              icon: Icon(Icons.remove_circle, color: Color(0xffFF414D)),
+                              onPressed: () {
+                                // Llama a la función para eliminar este jugador específico
+                                _removePlayer(index);
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  
+                ],
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(playerProvider.notifier);
-
-                // Opcional: Imprimir la información de los jugadores
-                final updatedPlayers = ref.read(playerProvider);
-                for (var player in updatedPlayers) {
-                  print('Jugador: ${player.name}');
-                }
-
-                // Navegar a la pantalla de reglas
-                gameMode == GameMode.custom
-                    ? GoRouter.of(context).go('/parametros')
-                    : GoRouter.of(context).go('/cartasasignadas');
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Jugar',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Lexend',
-                      fontWeight: FontWeight.w600), // Letra blanca
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                   Theme.of(context)
-                    .colorScheme
-                    .background
-                    .withOpacity(0.50), // Fondo negro
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30), // Bordes redondeados
-                ),
-                padding: EdgeInsets.symmetric(
-                    horizontal: 44, vertical: 10), // Padding interior del botón
-              ),
-            )
-          ],
-        ),
+          ),
+                Positioned(
+                right: screenSize.width * 0.05, // 5% del ancho de la pantalla
+                bottom: screenSize.height * 0.05, // 5% del alto de la pantalla
+               
+                  
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                          ref.read(playerProvider.notifier);
+                          
+                        // Opcional: Imprimir la información de los jugadores
+                        final updatedPlayers = ref.read(playerProvider);
+                        for (var player in updatedPlayers) {
+                          print('Jugador: ${player.name}');
+                        }
+                          
+                        // Navegar a la pantalla de reglas
+                        gameMode == GameMode.custom
+                            ? GoRouter.of(context).go('/parametros')
+                            : GoRouter.of(context).go('/cartasasignadas');
+                      
+                    },
+                  icon: Icon(Icons.play_arrow, color: Colors.white), // Icono de jugar
+                      label: Text('Jugar', style: TextStyle(color: Colors.white)),
+                     style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      textStyle: TextStyle(fontFamily: 'Lexend', fontWeight: FontWeight.w600),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                     ),
+                  ),
+                )
+        ],
       ),
     );
   }
 }
+
